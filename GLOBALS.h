0233eff27e171cd6df5d1cf98437d48f62cbb7c6
Shashwat Kansal <shashwatkansal05@gmail.com>
2026-08-13 18:22:24 +0530

    Create GLOBALS.h

---
+++ GLOBALS.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "TYPES.h"
+
+extern SensorData sensorData;
+extern FeatureData featureData;
+extern RiskLevel currentRisk;
+extern SystemMode currentMode;
+extern int systemHealth;
+
