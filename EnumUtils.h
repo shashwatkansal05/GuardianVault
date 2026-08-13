e09b742a77ef0bafdca7e74c16cb6bdbcec2c6a1
Shashwat Kansal <shashwatkansal05@gmail.com>
2026-08-13 18:37:45 +0530

    Create EnumUtils.h

---
+++ EnumUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Arduino.h>
+#include "TYPES.h"
+
+String riskLevelToString(RiskLevel level);
+
+String systemModeToString(SystemMode mode);
+
+String sensorStatusToString(SensorStatus status);
